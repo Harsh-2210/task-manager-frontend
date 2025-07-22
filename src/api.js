a25d45990cfc83a2f3d141ccaba1690b29a0e8fe
Harsh-2210 <harshsrivastavaofficial22@gmail.com>
2025-07-22 18:16:57 +0530

    frontend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://task-manager-backend-qsew.onrender.com/api',
+  baseURL: 'https://task-manager-backend-1-z3rg.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
