3ba57c6e56753b7c84ed3645b21b9b32e79a64e8
Harsh-2210 <harshsrivastavaofficial22@gmail.com>
2025-07-23 11:15:59 +0530

    Initial commit

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'http://localhost:5000/api',
-    withCredentials: false
+  withCredentials: false
 });
 
 API.interceptors.request.use((req) => {
